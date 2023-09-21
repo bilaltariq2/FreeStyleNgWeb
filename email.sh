e3d84bfa6c63d38131cfb456451cb1a7e635b26d
bilaltariq2 <91546263+bilaltariq2@users.noreply.github.com>
2023-09-21 14:59:20 +0500

    Update email.sh

--- email.sh
+++ email.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 email1="btariq@stellatechnology.com"
-echo "Hi, Please find the attach Amazon ECR Image vulnerabilities report. Thanks"| mail -s "ECR Image Scan Report" -A `ls -t /home/btariq/scan_report* | head -1` $email1
+echo "Hi, Please find the attach Amazon ECR Image vulnerabilities report. Thanks"| mail -s "ECR Image Scan Report" -A `ls -t /var/lib/jenkins/workspace/nginx-ecr/scan_report* | head -1` $email1
